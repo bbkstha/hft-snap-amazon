cfac2d82070b9ff6c0bf3bcf38cd49afd843fa39
bbkstha <bbkstha@gmail.com>
2017-12-06 02:15:50 -0700

    made major changes.

---
+++ src/main/scala/com/csu/fa17/cs535/tp/DataETL.scala
@@ -0,0 +1,63 @@
+package com.csu.fa17.cs535.tp
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+object HFTMain {
+
+  case class sch(userID: String, itemID: String, rating: String, time: String, wordCount: String, reviewText: String)
+
+    def main(args: Array[String]) {
+
+      val spark = SparkSession
+        .builder()
+        .appName("ALS_Train")
+        .master("local")
+        .getOrCreate()
+      import spark.implicits._
+
+      val rawData = spark.read.json("/home/bbkstha/CSU/I/BigData/Project/data/Musical_Instruments_5.json").toDF();
+      rawData.show(5)
+
+      val schemaString = "userID itemID rating time wordCount reviewText"
+      // Generate the schema based on the string of schema
+      val fields = schemaString.split(" ")
+        .map(fieldName => StructField(fieldName, StringType, nullable = true))
+      val schema = StructType(fields)
+
+
+      val x = rawData.map(r=>(r.apply(5).toString,
+                              r.apply(0).toString,
+                              r.apply(2).toString,
+                              r.apply(8).toString,
+                              r.apply(3).toString.split(" ").length.toString,
+                              r.apply(3).toString.replaceAll("[^a-zA-Z]", " "))).toDF()
+
+
+        val b = x.map(c=>(c.apply(0).toString+" "+c.apply(1).toString+" "+c.apply(2).toString+" "+c.apply(3).toString
+                  +" "+c.apply(4).toString+" "+c.apply(5).toString))
+
+      val rawDataForALS =  x.map(c=>(c.apply(0).toString, c.apply(1).toString, c.apply(2).toString, c.apply(3).toString)).toDF()
+
+      //rawDataForALS.show(10)
+      //rawDataForALS.coalesce(1).write.format("com.databricks.spark.csv").option("header", "false").save("/home/bbkstha/CSU/I/BigData/Project/data/Musical_Instruments_5.csv")
+
+
+      b.coalesce(1).write.text("/home/bbkstha/CSU/I/BigData/Project/data/Musical_Instruments_5_NEW.txt");
+      //x.show(5)
+
+
+
+//         x.coalesce(1).write.text().format("t")sa
+//          .write.format("com.databricks.spark.csv")
+//          .option("header", "false")
+//          .save("mydata.csv")
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/scala/com/csu/fa17/cs535/tp/als/TestHFTGeneratedRating.scala
@@ -0,0 +1,98 @@
+package com.csu.fa17.cs535.tp.als
+
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.sql.SparkSession
+
+
+
+case class NewRatingClass(userId: String, itemId: String, rating: Double, timestamp: Long)
+case class Ratings(userNumber: Int,itemNumber: Int, rating: Double)
+
+
+
+object TestHFTGeneratedRating {
+
+
+  def parseRating(str: String): NewRatingClass = {
+    val fields = str.split(",")
+    assert(fields.size == 4)
+    NewRatingClass(fields(0).toString(), fields(1).toString, fields(2).toDouble, fields(3).toLong)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("ALS_Train")
+      .master("local")
+      .getOrCreate()
+    import spark.implicits._
+
+
+    val execStart = System.currentTimeMillis()
+    //val fileName =args.apply(0)
+    val ratingCSV = "/home/bbkstha/CSU/I/BigData/Project/data/Musical_Instruments_5.csv/1.csv"//"hdfs://boise:31701/ "+fileName+".txt"
+    //val savedModelFile = "hdfs://boise:31701/"+fileName+"SavedModel.txt"
+
+
+    val oldRatings = spark.read.textFile(ratingCSV).map(parseRating).toDF()
+
+    //val data = spark.read.textFile(ratingCSV)
+    //val tempRating = data.map(_.split(',') match { case Array(userId, itemId, rating, timestamp) =>
+    //NewRatingClass(userId.toString, itemId.toString, rating.toDouble, timestamp.toLong)
+    //})
+
+    val stringindexer = new StringIndexer()
+      .setInputCol("userId")
+      .setOutputCol("userNumber")
+    val modelc = stringindexer.fit(oldRatings)
+    val  df0 = modelc.transform(oldRatings)
+    val newdf0 = df0.drop("userId")
+
+    val stringindexer1 = new StringIndexer()
+      .setInputCol("itemId")
+      .setOutputCol("itemNumber")
+    val modelc1 = stringindexer1.fit(newdf0)
+    val  df1 = modelc1.transform(newdf0)
+    val ratings = df1.drop("itemId").drop("timestamp")
+
+    val Array(training, test) = ratings.randomSplit(Array(0.8, 0.1))
+
+    // Build the recommendation model using ALS on the training data
+    val als = new ALS()
+      .setMaxIter(10)
+      .setRegParam(0.01)
+      .setUserCol("userNumber")
+      .setItemCol("itemNumber")
+      .setRatingCol("rating")
+    val model = als.fit(training)
+
+    // Evaluate the model by computing the RMSE on the test data
+    // Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
+    model.setColdStartStrategy("drop")
+    val predictions = model.transform(test)
+    val evaluator = new RegressionEvaluator()
+      .setMetricName("mse")
+      .setLabelCol("rating")
+      .setPredictionCol("prediction")
+    val mse = evaluator.evaluate(predictions)
+    println(s"Mean-square error = $mse")
+
+
+    /*******************************************************************/
+
+    // Save and load model
+    //model.save(savedModelFile)
+    //val sameModel = MatrixFactorizationModel.load(spark.sparkContext, destinationFile)
+
+    val execTime = (System.currentTimeMillis() - execStart)/60000.0
+    println("The execution time is: "+execTime)
+    spark.stop()
+
+  }
+
+
+}
